e0a2454ec447eafc8de2b8e9c65fa984794b033a
A_Lippok <alippok@hotmail.co.uk>
2016-02-26 12:27:44 +0000

    Adds a params hash for initializing a new guest

--- Specs/guest_class_spec.rb
+++ Specs/guest_class_spec.rb
@@ -5,23 +5,39 @@ require_relative("../guest_class.rb")
 class TestGuest < MiniTest::Test
 
 def setup
- @guest1 = Guest.new("Jenny Hill", 120)
- @guest2 = Guest.new("Bob Pratt", 100)
+  # guest1_hash = {
+  #   room_wanted: "double_room",
+  #   beds_wanted: 2,
+  #   name: "Jenny Hill",
+  #   wallet: 120
+  # }
+  # @guest1 = Guest.new("Jenny Hill", 120, guest1_hash)
+  @guest1 = Guest.new(room_wanted: "double room", beds_wanted: 1, name: "Jenny Hill", money: 120)
+  # @guest2 = Guest.new("Bob Pratt", 100)
 end
 
 def test_if_guest_can_say_how_much_money
   assert_equal(120, @guest1.wallet)
 end
 
-def test_if_guest_can_take_money
-  assert_equal(150, @guest1.receive(30))
+def test_if_guest_can_tell_name
+  assert_equal("Jenny Hill", @guest1.name)
 end
 
-def test_if_guest_can_give_money_to_another_guest
-  result = @guest1.give_money(@guest2, 30)
-  assert_equal(90, result)
+# def test_if_guest_can_take_money
+#   assert_equal(150, @guest1.receive(30))
+# end
 
-end
+# def test_if_guest_can_give_money_to_another_guest
+#   result = @guest1.give_money(@guest2, 30)
+#   assert_equal(90, result)
+# end
+
+# def test_if_guest_can_tell_desired_room
+
+
+# end
+
 
 
 end
--- guest_class.rb
+++ guest_class.rb
@@ -1,25 +1,28 @@
 class Guest
 
-attr_reader(:name, :wallet)
+attr_reader(:name, :wallet, :room_wanted)
 
-def initialize(name, money)
-  @name = name
-  @wallet = money
- 
+def initialize(params)
+  @name = params[:name]
+  @wallet = params[:money]
+  @room_wanted = params[:room_wanted]
+  @beds_wanted = params[:beds_wanted]
 end
 
-def receive(amount)
-  @wallet += amount
-  return @wallet
-end
 
-def give_money(receiver, amount)
-  actual_money = @wallet -= amount
-  money_to_give = @wallet - actual_money
+
+# def receive(amount)
+#   @wallet += amount
+#   return @wallet
+# end
+
+# def give_money(receiver, amount)
+#   actual_money = @wallet -= amount
+#   money_to_give = @wallet - actual_money
   
-  receiver.receive(money_to_give)
-  return @wallet
-end
+#   receiver.receive(money_to_give)
+#   return @wallet
+# end
 
 
 

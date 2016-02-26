b0185a128e564ea40756026fa8ca918adc7b3abb
A_Lippok <alippok@hotmail.co.uk>
2016-02-26 14:59:22 +0000

    Adds a nights needed for stay to guest class

--- Specs/guest_class_spec.rb
+++ Specs/guest_class_spec.rb
@@ -12,8 +12,8 @@ def setup
   #   wallet: 120
   # }
   # @guest1 = Guest.new("Jenny Hill", 120, guest1_hash)
-  @guest1 = Guest.new(room_wanted: "double room", beds_wanted: 1, name: "Jenny Hill", money: 120)
-  # @guest2 = Guest.new("Bob Pratt", 100)
+  @guest1 = Guest.new(room_wanted: "double room", beds_wanted: 1, name: "Jenny Hill", money: 120, nights: 3)
+  @guest2 = Guest.new(name: "Bob Pratt", money: 100, beds_wanted: 2, room_wanted: "twin room", nights: 4)
 end
 
 def test_if_guest_can_say_how_much_money
@@ -24,19 +24,22 @@ def test_if_guest_can_tell_name
   assert_equal("Jenny Hill", @guest1.name)
 end
 
-# def test_if_guest_can_take_money
-#   assert_equal(150, @guest1.receive(30))
-# end
+def test_if_guest_can_take_money
+  assert_equal(150, @guest1.receive(30))
+end
 
-# def test_if_guest_can_give_money_to_another_guest
-#   result = @guest1.give_money(@guest2, 30)
-#   assert_equal(90, result)
-# end
+def test_if_guest_can_give_number_of_nights_wanted
+  assert_equal(3, @guest1.number_of_nights_needed)
+end
 
-# def test_if_guest_can_tell_desired_room
+def test_if_guest_can_give_money_to_another_guest
+  result = @guest1.give_money(@guest2, 30)
+  assert_equal(90, result)
+end
 
-
-# end
+def test_if_guest_can_tell_desired_room
+ assert_equal("double room", @guest1.room_wanted)
+end
 
 
 
--- guest_class.rb
+++ guest_class.rb
@@ -1,28 +1,27 @@
 class Guest
 
-attr_reader(:name, :wallet, :room_wanted)
+attr_reader(:name, :wallet, :room_wanted, :beds_wanted, :number_of_nights_needed)
 
 def initialize(params)
   @name = params[:name]
   @wallet = params[:money]
   @room_wanted = params[:room_wanted]
   @beds_wanted = params[:beds_wanted]
+  @number_of_nights_needed = params[:nights]
 end
 
 
 
-# def receive(amount)
-#   @wallet += amount
-#   return @wallet
-# end
+def receive(amount)
+  @wallet += amount
+  return @wallet
+end
 
-# def give_money(receiver, amount)
-#   actual_money = @wallet -= amount
-#   money_to_give = @wallet - actual_money
-  
-#   receiver.receive(money_to_give)
-#   return @wallet
-# end
+def give_money(receiver, amount)
+  @wallet -= amount
+  receiver.receive(amount)
+  return @wallet
+end
 
 
 

973777b9801907460525e6fc2f5040877622a4a2
A_Lippok <alippok@hotmail.co.uk>
2016-02-28 08:56:03 +0000

    Adds method to room to return customers name - have added parts that will not be in final method

--- Specs/double_room_class_spec.rb
+++ Specs/double_room_class_spec.rb
@@ -8,6 +8,7 @@ class TestDoubleRoom < MiniTest::Test
 def setup
   @room1 = DoubleRoom.new
   @guest1 = Guest.new(room_wanted: "double room", beds_wanted: 1, name: "Jenny Hill", money: 120, nights: 3)
+  @guest2 = Guest.new(name: "Bob Pratt", money: 100, beds_wanted: 2, room_wanted: "twin room", nights: 4)
 
 end
 
@@ -31,13 +32,24 @@ def test_if_room_can_tell_if_bed_is_available
   assert_equal("AVAILABLE", result)
 end
 
-def test_if_room_can_receive_a_customer
-result = @room1.receive_guest(@guest1)
-assert_equal("TAKEN", result)
+def test_if_room_can_receive_a_guest
+  result = @room1.receive_guest(@guest1)
+  assert_equal("UNAVAILABLE", result)
 end
 
+def test_if_room_can_receive_two_guests
+  result = @room1.receive_multi_guests(@guest1, @guest2)
+  assert_equal("UNAVAILABLE", result)
+end
 
-
+def test_if_room_can_return_guest_name
+  result = @room1.occupant_query(@guest1)
+  assert_equal("Jenny Hill", result)
+end
+# def test_if_room_can_give_guest_details
+#   result = @room1.occupant_query(@guest1)
+#   assert_equal({"Name"=>"Jenny Hill", "Nights remaining" => 3, "Ready for checkout"=>"No"}, result)
+# end
 
 end
 
--- double_room_class.rb
+++ double_room_class.rb
@@ -18,7 +18,7 @@ def room_available
   if @stats["Beds"].empty?
     return "AVAILABLE"
   else
-    return "TAKEN"
+    return "UNAVAILABLE"
   end
 end
 
@@ -27,12 +27,43 @@ def receive_guest(guest)
   return room_available
 end
 
+def receive_multi_guests(*guests)
+  guests.each{|guest| @stats["Beds"] << guest }
+  
+  return room_available
+end
+
+def occupant_query(guest)#This is only to have a customer in
+  @stats["Beds"] << guest#Done this to make sure there is a 
+  #guest acttually in the room. But won't be in the final 
+  #method. This method will only be run on UNAVAILABLE ROOMS
+  
+
+  guest_array = @stats["Beds"]
+  guest_name = ""
+  guest_array.each{|guest| guest_name = guest.name}
+  return guest_name
+end
 
 
 
+#   occupant_details = {
+#     "Name"=> "",
+#     "Nights remaining"=> 0,
+#     "Ready for checkout" => ""
+#   }
+#   occupant_details["Name"] = name
+#   occupant_details["Nights remaining"] = nights
 
+#   if occupant_details["Nights remaining"] == 0
+#     occupant_details["Ready for checkout"] = "Yes"
+#     return occupant_details
+#   elsif occupant_details["Nights remaining"]  > 0
+#     occupant_details["Ready for checkout"] = "No"
+#     return occupant_details
+#   end
 
-
+# end
 
 
 end
